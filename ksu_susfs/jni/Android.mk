907a6df9079ee341a024189a01c8dd0ed11fa081
simonpunk2016 <simonpunk2016@gmail.com>
2024-04-04 18:27:05 +0800

    make ksu_susfs tool statically linked as android_log is not needed. Also change [APP_PLATFORM] to [latest] to let user build their own target API level

--- ksu_susfs/jni/Android.mk
+++ ksu_susfs/jni/Android.mk
@@ -4,9 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := ksu_susfs
 LOCAL_SRC_FILES := main.c
-#LOCAL_LDFLAGS := -static
-
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_LDLIBS += -llog -lz
+LOCAL_LDFLAGS := -static
 
 include $(BUILD_EXECUTABLE)
--- ksu_susfs/jni/Application.mk
+++ ksu_susfs/jni/Application.mk
@@ -1,2 +1,2 @@
 APP_ABI := arm64-v8a armeabi-v7a x86 x86_64
-APP_PLATFORM := android-21
+APP_PLATFORM := latest
