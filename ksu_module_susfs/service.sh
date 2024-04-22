05cd928ed50d7fbdca90af52ad9fefb03aae9619
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-04-22 17:14:56 +0800

    module: hide custom changes
    - Personal changes
    - Hide every overlay, dex2oat, youtube, etc.
    - Fix syntax - add_sus_mount_type and add_sus_mount_path are now add_sus_mount
    
    Change-Id: b77f725a261127e98931ccc008222b4f191ff1a2
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- ksu_module_susfs/service.sh
+++ ksu_module_susfs/service.sh
@@ -4,3 +4,15 @@ MODDIR=${0%/*}
 
 SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
 
+until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
+until [ -d "/sdcard/Android" ]; do sleep 1; done
+
+# Overlayed/bind-mounted packages to hide
+packages="com.google.android.youtube com.google.android.apps.youtube.music"
+
+hide_app () {
+	for path in $(pm path $1 | cut -d: -f2) ; do ${SUSFS_BIN} add_sus_mount $path ; done
+}
+
+for i in $packages ; do hide_app $i ; done 
+
