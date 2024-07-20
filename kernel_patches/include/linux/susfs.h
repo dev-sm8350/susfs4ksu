12384c1e6e89d7a35da458cd4b720a024beee007
silvzr <117378129+silvzr@users.noreply.github.com>
2024-07-20 11:15:03 +0200

    Add 4.14 kernels support

--- kernel_patches/include/linux/susfs.h
+++ kernel_patches/include/linux/susfs.h
@@ -183,7 +183,7 @@ int susfs_add_sus_memfd(struct st_susfs_sus_memfd* __user user_info);
 int susfs_add_try_umount(struct st_susfs_try_umount* __user user_info);
 int susfs_set_uname(struct st_susfs_uname* __user user_info);
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,19,0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
 int susfs_sus_path_by_path(struct path* file, int* errno_to_be_changed, int syscall_family);
 #else
 int susfs_sus_path_by_path(const struct path* file, int* errno_to_be_changed, int syscall_family);
